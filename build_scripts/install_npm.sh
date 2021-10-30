ceb655ba15cdee25b70761314516bc91f3fe485f
Dave Compton <davecompton7@gmail.com>
2021-10-30 06:26:03 -0700

    improved install_npm

--- build_scripts/install_npm.sh
+++ build_scripts/install_npm.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 sudo apt-get install  -y npm
-sudo npm install -g npm@latest
+
+## to get latest node per: https://stackoverflow.com/questions/10075990/upgrading-node-js-to-latest-version
+sudo npm install -g n
+
+node --version
+
+# update to the latest node
+sudo n latest
+hash -r
+node --version
+
